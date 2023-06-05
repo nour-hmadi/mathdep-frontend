eb3bd0f7e3596c18072f3317056efad07cd79aff
nour <nour.hmadi.a@gmail.com>
2023-06-05 17:02:22 +0300

    comments testing

--- src/pages/MathCommunity.jsx
+++ src/pages/MathCommunity.jsx
@@ -3,12 +3,21 @@ import avatar from "../assests/imagetwo.png";
 import "../styles/MathCommunity.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { AiOutlineComment } from "react-icons/ai";
 
 const url = "https://mathdep.onrender.com/api/posts/";
 
 function MathCommunity() {
   const [info, setInfo] = useState([]);
   const [isLoggedin, setIsLoggedin] = useState(false);
+  const [comment, setComment] = useState([]);
+  const [newinfo, setNewInfo] = useState({
+
+    description:"",
+    postId:Number
+  });
+
+
 
   useEffect(() => {
     if (sessionStorage.getItem("token")) {
@@ -21,13 +30,38 @@ function MathCommunity() {
     getAllInfo();
   }, []);
 
+  //   const Comment = async()=>{
+  // try {
+  //  const response= await axios.get("http://localhost:5000/api/comments/")
+
+  //   setComment(response.data)
+  //   console.log("Comment",response.data[0].post)
+  // } catch (error) {
+  //   console.log(error)
+  // }
+  //   }
+
+  const addComment = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.post("http://localhost:5000/api/comments/", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setComment([...comment, response.data]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const getAllInfo = async () => {
     await axios
       .get(`${url}`)
       .then((response) => {
         setInfo(response.data);
 
-        // console.log("response.data: " + response.data.data);
+        console.log("responsedata: ", response.data);
       })
       .catch((error) => {
         console.error(`Error: ${error}`);
@@ -46,18 +80,34 @@ function MathCommunity() {
         </div>
         <div className="post_question">
           <p>{item.title}</p>
-
           <p>{item.description}</p>
         </div>
+       
       </div>
     );
   });
 
   return (
-    <div>
-    {isLoggedin ? <button>Add Post</button> : null }
+    <div className="math-community-page-container">
+      {isLoggedin ? <button>Add Post</button> : null}
       <h1>HELLO MATH COMM</h1>
       {cards}
+      <input
+          type="text"
+          value={newinfo.description}
+          onChange={(e) => setNewInfo({...newinfo, description:e.target.value})}
+        ></input>
+          <input
+          type="text"
+          value={newinfo.postId}
+          placeholder="post id"
+          onChange={(e) => setNewInfo({...newinfo, postId:e.target.value})}
+        ></input>
+        <button onClick={addComment}>add comment</button>
+        <button>
+          <AiOutlineComment />
+        </button>
+      
     </div>
   );
 }
